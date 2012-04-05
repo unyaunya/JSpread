686c042c0527ea1039d0a602b22784e3a6f39570
Watanabe, Katsutoshi <kys2917@gmail.com>
2012-04-05 17:04:18 +0900

    Added equals/hashCode method to CellFormat class.

--- src/com/unyaunya/spread/CellFormat.java
+++ src/com/unyaunya/spread/CellFormat.java
@@ -16,4 +16,41 @@ public class CellFormat implements Serializable {
 	public void setBackgroundColor(Color value) {
 		backgroundColor = value;
 	}
+
+	@Override
+	public boolean equals(Object value) {
+		if(value == null) {
+			return false;
+		}
+		if(value instanceof CellFormat) {
+			CellFormat val = (CellFormat)value;
+			if(this.getBackgroundColor() == null) {
+				if(val.getBackgroundColor() != null) {
+					return false;
+				}
+			}
+			else {
+				if(!val.getBackgroundColor().equals(val.getBackgroundColor())) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("CellFormat[backgroundColor=%s]", this.getBackgroundColor());
+	}
+
+	@Override
+	public int hashCode(){
+		final int multiplier = 37;
+		int result = 17;
+		result = multiplier * result + this.getBackgroundColor().hashCode();
+		if(this.getBackgroundColor()!=null) {
+			result = multiplier * result + this.getBackgroundColor().hashCode();
+		}
+		return result;
+	}
 }
